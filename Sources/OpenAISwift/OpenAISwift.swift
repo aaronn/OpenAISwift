7a217eaba3a2505a6ba89310ce33175e1dc14701
0xAaron <aaron@hyper.online>
2023-12-04 01:10:48 -0800

    Remove print.

--- Sources/OpenAISwift/OpenAISwift.swift
+++ Sources/OpenAISwift/OpenAISwift.swift
@@ -54,7 +54,6 @@ public class OpenAISwift {
 extension OpenAISwift {
 
     func makeRequest(request: URLRequest, completionHandler: @escaping (Result<Data, Error>) -> Void) {
-        print(String(data:request.httpBody!, encoding:.utf8))
         let session = config.session
         let task = session.dataTask(with: request) { (data, response, error) in
             if let error = error {
